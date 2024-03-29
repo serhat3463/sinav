51c11dc42046b223070e41dc1c4a382a9d756b50
serhat3463 <165370579+serhat3463@users.noreply.github.com>
2024-03-29 13:05:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const btn = document.getElementById('changeColor');
+
+
+let randomNumber = () => {
+    let randomNum = (Math.floor(Math.random() * 256));
+    return randomNum;
+}
+
+let changeColor = () => {
+    let randomColor = `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
+    document.body.style.backgroundColor = randomColor;
+}
+
+btn.addEventListener('click', changeColor);
+window.addEventListener('load', changeColor);
